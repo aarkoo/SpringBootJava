c04704beebd7e05b33cb6d2813c0e90ad305801b
akash <aakashh92@gmail.com>
2022-06-11 18:03:15 +0530

    spring1

---
+++ src/main/java/com/example/springbootcode/SpringbootcodeApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcodeApplication.class, args);
+		System.out.println("_____________________---------starting server--------------_______________________");
+		System.out.println("starting spring boot");
+		
+	}
+
+}
